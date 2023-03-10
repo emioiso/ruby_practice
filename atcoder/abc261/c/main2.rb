02099e195d2b6d29563cc86a125728932dfc1513
emioiso <jijjjiijiijijiji@gmail.com>
2023-03-10 18:05:33 +0900

    二回目C - NewFolder(1)

---
+++ atcoder/abc261/c/main2.rb
@@ -0,0 +1,19 @@
+n = gets.to_i
+# puts n
+s = readlines
+# puts s
+
+count = {}
+s.each do |b|
+	if count[b]==nil then
+		count[b]=1
+	else
+		count[b]+=1
+	end
+		if count[b]>=2 then
+			str="#{b.chomp}(#{count[b]-1})"
+			puts str
+		else
+			puts b
+		end
+	end
