c8e888630556ab67fe37fb99aa36a227e61f2501
emioiso <jijjjiijiijijiji@gmail.com>
2023-03-10 18:03:19 +0900

    三回目C - NewFolder(1)

--- atcoder/abc261/c/main.rb
+++ atcoder/abc261/c/main.rb
@@ -1,19 +1,19 @@
 n = gets.to_i
-# puts n
+# p n
 s = readlines
 # puts s
 
 count = {}
 s.each do |b|
-	if count[b]==nil then
-		count[b]=1
+	if count[b] == nil then
+		count[b] = 1
 	else
-		count[b]+=1
+		count[b] += 1
 	end
-		if count[b]>=2 then
-			str="#{b.chomp}(#{count[b]-1})"
+		if count[b] >= 2 then
+			str = "#{b.chomp}(#{count[b] - 1})"
 			puts str
 		else
-			puts b
+			puts b.chomp
 		end
 	end
