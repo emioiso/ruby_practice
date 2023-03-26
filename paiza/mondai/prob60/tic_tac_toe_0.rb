0090404c016f24bea14ccc956e615df2ae9105e2
emioiso <jijjjiijiijijiji@gmail.com>
2023-03-26 17:02:12 +0900

    「文字列の出力 Ruby編」

--- paiza/mondai/prob60/tic_tac_toe_0.rb
+++ paiza/mondai/prob60/tic_tac_toe_0.rb
@@ -1,14 +1,22 @@
-str1 = gets
-puts str1
+# str = gets.chomp
+# puts str
 
-str2 = gets
-puts str2
+for i in 0..5
+    str = gets
+    puts str
+end
 
-str3 = gets
-puts str3
+# str1 = gets
+# puts str1
 
-str4 = gets
-puts str4
+# str2 = gets
+# puts str2
 
-str5 = gets
-puts str5
+# str3 = gets
+# puts str3
+
+# str4 = gets
+# puts str4
+
+# str5 = gets
+# puts str5
