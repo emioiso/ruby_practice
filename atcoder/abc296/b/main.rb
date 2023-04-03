c74c57a87f344f553b36768247e4c6aeb664cf69
emioiso <jijjjiijiijijiji@gmail.com>
2023-04-03 13:36:01 +0900

    B - Chessboard

---
+++ atcoder/abc296/b/main.rb
@@ -0,0 +1,187 @@
+# Your code here!
+s1 = gets.chomp
+# puts s1
+
+for i in 0..8
+    if s1[i] == '*' && i == 0
+        puts "a8"
+    elsif s1[i] == '*' && i == 1
+        puts 'b8'
+    elsif s1[i] == '*' && i == 2
+        puts 'c8'
+    elsif s1[i] == '*' && i == 3
+        puts 'd8'
+    elsif s1[i] == '*' && i == 4
+        puts 'e8'
+    elsif s1[i] == '*' && i == 5
+        puts 'f8'
+    elsif s1[i] == '*' && i == 6
+        puts 'g8'
+    elsif s1[i] == '*' && i == 7
+        puts 'h8'
+    end
+end
+
+s2 = gets.chomp
+# puts s2
+
+for i in 0..8
+    if s2[i] == '*' && i == 0
+        puts 'a7'
+    elsif s2[i] == '*' && i == 1
+        puts 'b7'
+    elsif s2[i] == '*' && i == 2
+        puts 'c7'
+    elsif s2[i] == '*' && i == 3
+        puts 'd7'
+    elsif s2[i] == '*' && i == 4
+        puts 'e7'
+    elsif s2[i] == '*' && i == 5
+        puts 'f7'
+    elsif s2[i] == '*' && i == 6
+        puts 'g7'
+    elsif s2[i] == '*' && i == 7
+        puts 'h7'
+    end
+end
+
+s3 = gets.chomp
+# puts s3
+
+for i in 0..8
+    if s3[i] == '*' && i == 0
+        puts 'a6'
+    elsif s3[i] == '*' && i == 1
+        puts 'b6'
+    elsif s3[i] == '*' && i == 2
+        puts 'c6'
+    elsif s3[i] == '*' && i == 3
+        puts 'd6'
+    elsif s3[i] == '*' && i == 4
+        puts 'e6'
+    elsif s3[i] == '*' && i == 5
+        puts 'f6'
+    elsif s3[i] == '*' && i == 6
+        puts 'g6'
+    elsif s3[i] == '*' && i == 7
+        puts 'h6'
+    end
+end
+
+s4 = gets.chomp
+# puts s4
+
+for i in 0..8
+    if s4[i] == '*' && i == 0
+        puts 'a5'
+    elsif s4[i] == '*' && i == 1
+        puts 'b5'
+    elsif s4[i] == '*' && i == 2
+        puts 'c5'
+    elsif s4[i] == '*' && i == 3
+        puts 'd5'
+    elsif s4[i] == '*' && i == 4
+        puts 'e5'
+    elsif s4[i] == '*' && i == 5
+        puts 'f5'
+    elsif s4[i] == '*' && i == 6
+        puts 'g5'
+    elsif s4[i] == '*' && i == 7
+        puts 'h5'
+    end
+end
+
+s5 = gets.chomp
+# puts s5
+
+for i in 0..8
+    if s5[i] == '*' && i == 0
+        puts 'a4'
+    elsif s5[i] == '*' && i == 1
+        puts 'b4'
+    elsif s5[i] == '*' && i == 2
+        puts 'c4'
+    elsif s5[i] == '*' && i == 3
+        puts 'd4'
+    elsif s5[i] == '*' && i == 4
+        puts 'e4'
+    elsif s5[i] == '*' && i == 5
+        puts 'f4'
+    elsif s5[i] == '*' && i == 6
+        puts 'g4'
+    elsif s5[i] == '*' && i == 7
+        puts 'h4'
+    end
+end
+
+s6 = gets.chomp
+# puts s6
+
+for i in 0..8
+    if s6[i] == '*' && i == 0
+        puts 'a3'
+    elsif s6[i] == '*' && i == 1
+        puts 'b3'
+    elsif s6[i] == '*' && i == 2
+        puts 'c3'
+    elsif s6[i] == '*' && i == 3
+        puts 'd3'
+    elsif s6[i] == '*' && i == 4
+        puts 'e3'
+    elsif s6[i] == '*' && i == 5
+        puts 'f3'
+    elsif s6[i] == '*' && i == 6
+        puts 'g3'
+    elsif s6[i] == '*' && i == 7
+        puts 'h3'
+    end
+end
+
+
+s7 = gets.chomp
+# puts s7
+
+for i in 0..8
+    if s7[i] == '*' && i == 0
+        puts 'a2'
+    elsif s7[i] == '*' && i == 1
+        puts 'b2'
+    elsif s7[i] == '*' && i == 2
+        puts 'c2'
+    elsif s7[i] == '*' && i == 3
+        puts 'd2'
+    elsif s7[i] == '*' && i == 4
+        puts 'e2'
+    elsif s7[i] == '*' && i == 5
+        puts 'f2'
+    elsif s7[i] == '*' && i == 6
+        puts 'g2'
+    elsif s7[i] == '*' && i == 7
+        puts 'h2'
+    end
+end
+
+
+s8 = gets.chomp
+# puts s8
+
+for i in 0..8
+
+    if s8[i] == '*' && i == 0
+        puts 'a1'
+    elsif s8[i] == '*' && i == 1
+        puts 'b1'
+    elsif s8[i] == '*' && i == 2
+        puts 'c1'
+    elsif s8[i] == '*' && i == 3
+        puts 'd1'
+    elsif s8[i] == '*' && i == 4
+        puts 'e1'
+    elsif s8[i] == '*' && i == 5
+        puts 'f1'
+    elsif s8[i] == '*' && i == 6
+        puts 'g1'
+    elsif s8[i] == '*' && i == 7
+        puts 'h1'
+    end
+end
